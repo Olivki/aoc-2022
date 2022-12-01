2e4de5ae795fa45159a9de22fb8dd30165cf0705
Oliver Berg <oliver@berg.moe>
2022-12-01 21:55:27 +0100

    Day #01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>) = buildList {
+        val inventory = hashSetOf<Int>()
+        for (line in input) {
+            if (line.isNotBlank()) {
+                inventory += line.toInt()
+            } else {
+                add(inventory.sum())
+                inventory.clear()
+            }
+        }
+        if (inventory.isNotEmpty()) add(inventory.sum())
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>) = part1(input)
+        .sortedDescending()
+        .take(3)
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // p1
+    check(part1(testInput) == listOf(6_000, 4_000, 11_000, 24_000, 10_000))
+    check(part1(testInput).max() == 24_000)
+    // p2
+    check(part2(testInput) == listOf(24_000, 11_000, 10_000))
+    check(part2(testInput).sum() == 45_000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input).max())
+    println(part2(input).sum())
 }
