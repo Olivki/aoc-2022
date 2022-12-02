53c9196b5005bf76a4f1005cc62f1a4430054e43
Oliver Berg <oliver@berg.moe>
2022-12-02 16:38:56 +0100

    Refactor inputs handling
    Moved all inputs to its own directory for better organizing

--- src/Utils.kt
+++ src/Utils.kt
@@ -3,7 +3,7 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
-fun readInput(name: String) = Path("./src/$name.txt").readLines()
+fun readInput(name: String) = Path("./inputs/$name.txt").readLines()
 
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
